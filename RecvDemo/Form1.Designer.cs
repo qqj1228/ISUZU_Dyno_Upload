184a81625c426ff14a413a3c465c21fedfef6418
Cash Qian <qqj1228@gmail.com>
2020-01-15 17:09:03 +0800

    added recvDemo

---
+++ RecvDemo/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace RecvDemo {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.txtBoxVIN = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtBoxRecv = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtBoxVIN
+            // 
+            this.txtBoxVIN.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBoxVIN.Location = new System.Drawing.Point(13, 13);
+            this.txtBoxVIN.Name = "txtBoxVIN";
+            this.txtBoxVIN.Size = new System.Drawing.Size(278, 21);
+            this.txtBoxVIN.TabIndex = 0;
+            this.txtBoxVIN.Text = "VIN";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(297, 13);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtBoxRecv
+            // 
+            this.txtBoxRecv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBoxRecv.Location = new System.Drawing.Point(13, 41);
+            this.txtBoxRecv.Multiline = true;
+            this.txtBoxRecv.Name = "txtBoxRecv";
+            this.txtBoxRecv.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtBoxRecv.Size = new System.Drawing.Size(359, 108);
+            this.txtBoxRecv.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.txtBoxRecv);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtBoxVIN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxVIN;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtBoxRecv;
+    }
+}
+
---
+++ RecvDemo/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RecvDemo {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e) {
+            TcpClient client = new TcpClient("127.0.0.1", 50001);
+            NetworkStream clientStream = client.GetStream();
+            byte[] sendMessage;
+            byte[] recv = new byte[4096];
+            int bytesRead;
+            string strRecv;
+
+            // 发送VIN号
+            sendMessage = Encoding.UTF8.GetBytes(this.txtBoxVIN.Text);
+            clientStream.Write(sendMessage, 0, sendMessage.Length);
+            clientStream.Flush();
+
+            // 接收服务器返回消息
+            // 本demo采用同步接收函数，代码会阻塞在接收函数这里，直到收到返回服务器消息
+            // 实际使用的时候需要调用异步接收函数，或者另开一个接收线程
+            bytesRead = clientStream.Read(recv, 0, 4096);
+            if (bytesRead == 0) {
+                this.txtBoxVIN.Text = "接收服务器返回VIN号格式出错";
+                return;
+            }
+            strRecv = Encoding.UTF8.GetString(recv, 0, bytesRead);
+            // 这里收到的“200”字符串不需要解析
+            if (strRecv == "200") {
+                bytesRead = clientStream.Read(recv, 0, 4096);
+                if (bytesRead == 0) {
+                    this.txtBoxVIN.Text = "接收服务器返回参数数据出错";
+                    return;
+                }
+                strRecv = Encoding.UTF8.GetString(recv, 0, bytesRead);
+            }
+            // 本demo只是简单的将收到的JSON字符串显示在界面上，实际使用时需要解析这个JSON字符串
+            this.txtBoxRecv.Text = strRecv;
+        }
+    }
+}
